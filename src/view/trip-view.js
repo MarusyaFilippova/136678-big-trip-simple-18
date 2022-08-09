89837be74cd5a0ab976434a113f05c2375c7f1d6
maria <filippovamarusya@gmail.com>
2022-08-09 12:35:54 +0200

    module1-task3: update template

--- src/view/trip-view.js
+++ src/view/trip-view.js
@@ -14,6 +14,7 @@ const createTripTemplate = () => (`
           &mdash;
           <time class="event__end-time" datetime="2019-03-18T11:00">11:00</time>
         </p>
+        <p class="event__duration">30M</p>
       </div>
       <p class="event__price">
         &euro;&nbsp;<span class="event__price-value">20</span>
@@ -26,6 +27,12 @@ const createTripTemplate = () => (`
           <span class="event__offer-price">20</span>
         </li>
       </ul>
+      <button class="event__favorite-btn event__favorite-btn--active" type="button">
+        <span class="visually-hidden">Add to favorite</span>
+        <svg class="event__favorite-icon" width="28" height="28" viewBox="0 0 28 28">
+          <path d="M14 21l-8.22899 4.3262 1.57159-9.1631L.685209 9.67376 9.8855 8.33688 14 0l4.1145 8.33688 9.2003 1.33688-6.6574 6.48934 1.5716 9.1631L14 21z"/>
+        </svg>
+      </button>
       <button class="event__rollup-btn" type="button">
         <span class="visually-hidden">Open event</span>
       </button>
